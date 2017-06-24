63f4c6d744ceb9828c11a680bb0b9bbd8e37becd
anoopmundathan <anoop.mundathan@gmail.com>
2017-06-24 12:06:51 +0100

    Add hamburger Icon

---
+++ src/components/page/header/Hamburger.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hamburger = () => {
+	return(
+		<div className='hamburger-container'>
+			<div className='hamburger'>
+				<div className='hamburger-top'>
+				</div>
+				<div className='hamburger-middle'>
+				</div>
+				<div className='hamburger-bottom'>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Hamburger;
--- src/components/page/header/Header.js
+++ src/components/page/header/Header.js
@@ -3,14 +3,16 @@ import React from 'react';
 import Logo from './Logo';
 import Title from './Title';
 import NavBar from './NavBar';
+import Hamburger from './Hamburger';
 
 const Header = () => {
 	return(
 		<header>
 			<div className='header-container'>
-				<div className='logo-title-container'>
+				<div className='logo-title-ham-container'>
 					<Logo/>
 					<Title/>
+					<Hamburger/>
 				</div>
 				<div className='navbar-container'>
 					<NavBar/>
